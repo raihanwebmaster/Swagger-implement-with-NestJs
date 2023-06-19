2237a5cbd68fa825aa363c84f27875ec4c2fa4cd
Raihan Uddin <raihanemon2015@gmail.com>
2023-06-19 12:20:52 +0200

    swagger implement with nestjs

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsAlphanumeric, MaxLength } from 'class-validator';
+export class CreateUserDto {
+  @ApiProperty()
+  @IsAlphanumeric()
+  @MaxLength(10)
+  name: string;
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import {
+  ApiTags,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiQuery,
+  ApiNotFoundResponse,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
+
+@ApiTags('users')
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @ApiOkResponse({ type: User, isArray: true })
+  @ApiQuery({ name: 'name', required: false })
+  @Get()
+  getUsers(@Query('name') name: string): User[] {
+    return this.usersService.findAll(name);
+  }
+
+  @ApiOkResponse({ type: User, description: 'The User' })
+  @ApiNotFoundResponse()
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number): User {
+    const user = this.usersService.findById(id);
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+
+  @ApiCreatedResponse({ type: User })
+  @ApiBadRequestResponse()
+  @Post()
+  createUser(@Body() body: CreateUserDto): User {
+    return this.usersService.createUser(body);
+  }
+}
